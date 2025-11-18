91057d487fee1d89ca7f2a7a328d93bd9424a9a5
Justus1234567 <152340067+Justus1234567@users.noreply.github.com>
2025-11-18 02:37:28 -0600

    Just before runnable and threads

---
+++ StarWars_game/src/Main/GameWindow.java
@@ -0,0 +1,21 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class GameWindow {
+    private JFrame jframe;
+
+    @SuppressWarnings("static-access")
+    public GameWindow(GamePanel gamePanel){
+
+        jframe = new JFrame();
+
+        jframe.setSize(400,400);
+        jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
+        jframe.add(gamePanel);
+        jframe.setLocationRelativeTo(null);
+        jframe.setVisible(true);
+
+    }
+    
+}
